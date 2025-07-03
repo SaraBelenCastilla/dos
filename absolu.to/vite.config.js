f92b0538735d91433246c284125ea694c5a10f9c
SaraBelenCastilla <saracastillalagata@gmail.com>
2025-07-03 11:35:24 +0200

    11.30

--- absolu.to/vite.config.js
+++ absolu.to/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   base: './', // Asegúrate de que esto sea correcto para tu caso
 })
